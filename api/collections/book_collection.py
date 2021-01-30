25fcbaed31e3e1ad22736903552d0e907dc8608d
Recep <recep4sirin@gmail.com>
2021-01-30 16:31:10 +0300

    minor changes applied

--- api/collections/book_collection.py
+++
@@ -1,8 +0,0 @@
-from api.helpers.mongodb_helper import MongodbHelper
-
-
-class BookCollection(MongodbHelper):
-
-    def __init__(self, collection_name):
-        super().__init__()
-        self._name = collection_name
