e03612f914ef556baff5e2e0e465f672bdfcc865
Recep <recep4sirin@gmail.com>
2021-01-30 17:11:14 +0300

    flask-app file implemented

---
+++ api/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Api
+from models.players import initialize_db
+from routing.routes import initialize_routes
+from helpers.yml_parser import parser
+app = Flask(__name__)
+api = Api(app)
+
+app.config['MONGODB_SETTINGS'] = {
+    'host': parser()['database']['mongo']['dsn']
+}
+
+
+def initialize():
+    initialize_db(app)
+    initialize_routes(api)
+
+
+def start_app():
+    initialize()
+    app.run()
+
+
+if __name__ == '__main__':
+    start_app()
