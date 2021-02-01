7efc0cf8e752239bd334dc38c0e6245b1515a1a0
Recep <recep4sirin@gmail.com>
2021-02-01 23:31:57 +0300

    view created and optimized with get and post methods

--- organizations/api/views.py
+++ organizations/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import ChessGame
+from .serializers import GameSerializer
+from rest_framework.generics import ListAPIView, GenericAPIView, CreateAPIView
 
-# Create your views here.
+
+class ChessGameView(ListAPIView, CreateAPIView, GenericAPIView):
+    queryset = ChessGame.objects.all()
+    serializer_class = GameSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
