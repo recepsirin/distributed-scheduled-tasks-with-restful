31bb6c2fd860d2c61dccc2701c1f94049e74af7a
Recep <recep4sirin@gmail.com>
2021-02-01 23:26:11 +0300

    project optimized and enabled admin

--- organizations/api/admin.py
+++ organizations/api/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import ChessGame
 
-# Register your models here.
+admin.site.register(ChessGame)
--- organizations/organizations/settings.py
+++ organizations/organizations/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +77,13 @@ WSGI_APPLICATION = 'organizations.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'organization',
+        'USER': 'postgres',
+        'HOST': 'localhost',
     }
 }
+
 
 
 # Password validation
