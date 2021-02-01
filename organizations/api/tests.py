722d31c88f2a8c22e91d420c4f9fb9e0058980f3
Recep <recep4sirin@gmail.com>
2021-02-02 00:52:40 +0300

    api tests added

--- organizations/api/tests.py
+++ organizations/api/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .models import ChessGame
+from rest_framework.test import APIClient
+from django.urls import reverse
+from rest_framework import status
 
 
 class ChessGameModelTest(TestCase):
@@ -20,3 +23,29 @@ class ChessGameModelTest(TestCase):
 
         with self.assertRaises(Exception):
             ChessGame.objects.create(white=white, black=black, competition_date=competition_date)
+
+
+class ChessGameEndpointTest(TestCase):
+
+    def setUp(self):
+        """Setting up the client and sample payload"""
+        self.client = APIClient()
+
+        self.chess_game = {'white': 1442921, 'black': 1442392, 'competition_date': '2021-12-31'}
+        self.response = self.client.post(reverse('chess-game'), self.chess_game, format="json")
+
+    def test_create_organization(self):
+        """Test the organizations endpoint's post method by asserting http status code"""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_organizations(self):
+        """"Test for get request that listing organizations"""
+        response = self.client.get(reverse("chess-game"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_filtering_specific_organization(self):
+        """Test for filtering specific organization"""
+        response = self.client.get(reverse('chess-game'), kwargs={'_id': 1})
+        competition_date = response.data['results'][0]['competition_date']
+
+        self.assertEqual(competition_date, self.chess_game.get('competition_date'))
