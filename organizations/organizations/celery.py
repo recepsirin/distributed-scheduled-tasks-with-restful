aef3d564740c2cb0431fe3337251e9ef0089978c
Recep <recep4sirin@gmail.com>
2021-02-07 22:51:39 +0300

    celery re-configured and celery-results added

--- organizations/organizations/celery.py
+++ organizations/organizations/celery.py
@@ -4,6 +4,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'organizations.settings')
 
-app = Celery('celery')
+app = Celery('organizations')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
