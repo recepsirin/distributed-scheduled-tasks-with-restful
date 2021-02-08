b44a33e1b01923e28edeb668317f0412d737a031
Recep <recep4sirin@gmail.com>
2021-02-08 21:57:38 +0300

    players ids' field updated with positive field and str func added to chess-game

--- organizations/api/models.py
+++ organizations/api/models.py
@@ -3,10 +3,13 @@ from django.db import models
 
 class ChessGame(models.Model):
     _id = models.AutoField(primary_key=True)
-    white = models.IntegerField(null=False)
-    black = models.IntegerField(null=False)
+    white = models.PositiveIntegerField(null=False)
+    black = models.PositiveIntegerField(null=False)
     competition_date = models.DateField()
     created_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s vs %s >> %s" % (self.white, self.black, self.competition_date)
 
 
 class CompetitionResult(models.Model):
