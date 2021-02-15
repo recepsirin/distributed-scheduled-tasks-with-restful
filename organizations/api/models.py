92836cd90f5bff4239e87308ed38f3ed53e2827f
Recep <recep4sirin@gmail.com>
2021-02-15 21:20:52 +0300

    added state field

--- organizations/api/models.py
+++ organizations/api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+STATES = (("not-played", "NOT PLAYED"), ("playing", "PLAYING"), ("played", "PLAYED"))
+
 
 class ChessGame(models.Model):
     _id = models.AutoField(primary_key=True)
@@ -7,6 +9,7 @@ class ChessGame(models.Model):
     black = models.PositiveIntegerField(null=False)
     competition_date = models.DateField()
     created_date = models.DateTimeField(auto_now=True)
+    state = models.CharField(choices=STATES, max_length=15, default="not-played", null=False)
 
     def __str__(self):
         return "%s vs %s >> %s" % (self.white, self.black, self.competition_date)
